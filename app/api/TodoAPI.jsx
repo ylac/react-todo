771428cc75a38be26487f7e315a7bf9db6f8bc48
Caly <caly@inventa.com.au>
2017-02-26 19:53:25 +1100

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,35 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    })
+    // Filter by searchText
+    if (searchText.length > 0) {
+      var searchStr = searchText.toLowerCase();
+      filteredTodos = filteredTodos.filter((todo) => {
+        var todoText = todo.text.toLowerCase();
+        var todoFoundIndex = todoText.indexOf(searchStr);
+        return (todoFoundIndex >= 0) ? true : false;
+      });
+    }
+
+    // Sort todos with uncompleted first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -45,6 +45,47 @@ describe('TodoAPI', () => {
       localStorage.setItem('todos', JSON.stringify(todo));
       expect(TodoAPI.getTodos()).toEqual(todo);
     });
+  });
 
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: 'Buy chicken',
+      completed: true
+    }, {
+      id: 2,
+      text: 'Eat chicken',
+      completed: true
+    }, {
+      id: 3,
+      text: 'Sell chicken',
+      completed: false
+    }];
+    it('should return all todos if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should only return uncompleted items if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should return all todos if searchText empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should filter todos by searchText', () => {
+      var searchText = 'eat';
+      var filteredTodos = TodoAPI.filterTodos(todos, true, searchText);
+      expect(filteredTodos.length).toBe(1);
+    });
   });
 });
